e6d2f9920be202705692304730c35849a6450aab
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-03 21:27:01 +0100

    completed goals rendering

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+let goalsContainer = document.querySelector("#goalsContainer");
 const updateTimeAndDay = () => {
   let currentTime = new Date().toUTCString().substring(17, 25);
   const currentDay = new Date().toLocaleDateString("en-US", {
@@ -8,7 +9,6 @@ const updateTimeAndDay = () => {
   let currentTimeElement = document.querySelector("#currentTime");
   currentTimeElement.textContent = currentTime;
   currentDayElement.textContent = currentDay;
-  let cardContainer = document.querySelector("#cardContainer");
 };
 setInterval(() => updateTimeAndDay(), 1000);
 updateTimeAndDay();
@@ -16,7 +16,8 @@ updateTimeAndDay();
 let goals = [
   {
     title: "Open source contribution ",
-    content: "i plan on contributing to more Open Source Projects to grow my skills",
+    content:
+      "i plan on contributing to more Open Source Projects to grow my skills",
   },
   {
     title: "Developer role ",
@@ -25,11 +26,17 @@ let goals = [
   },
   {
     title: "Enhance my skills ",
-    content: "I plan on improving my skills in both mobile, frontend and backend environment",
+    content:
+      "I plan on improving my skills in both mobile, frontend and backend environment",
   },
   {
     title: "Network with other people",
     content: "Build connections with peers to learn and grow together",
+  },
+  {
+    title: "Enhance problem-solving skills with algorithms and data structures",
+    content:
+      "I would have significantly Improved my ability to solve complex problems efficiently.",
   },
   {
     title: " AI technology ",
@@ -37,15 +44,37 @@ let goals = [
   },
   {
     title: " Start a tech blog to share knowledge and experiences",
-    content: "I want to Write articles to document my journey and help others learn.",
+    content:
+      "I want to Write articles to document my journey and help others learn.",
   },
   {
     title: " Participate in hackathons and coding competitions ",
-    content: "I plan to Engage in competitive coding to sharpen problem-solving skills.",
+    content:
+      "I plan to Engage in competitive coding to sharpen problem-solving skills.",
   },
 
   {
     title: "Expansion of knowledge ",
-    content: "i wish to expand my knowledge in web development and stay updated with the latest trends",
+    content:
+      "i wish to expand my knowledge in web development and stay updated with the latest trends",
+  },
+  {
+    title: "Build and contribute to personal and community-driven project",
+    content:
+      "i will Collaborate on projects that make a positive impact on the developer community and the world at large",
   },
 ];
+// goals.map((goal)=>)
+
+goalsContainer.innerHTML = "";
+goals.map((goal) => {
+  let card = document.createElement("div");
+  card.className = "card";
+  let title = document.createElement("h2");
+  title.textContent = goal.title;
+  card.appendChild(title);
+  let content = document.createElement("span");
+  content.textContent = goal.content;
+  card.appendChild(content);
+  goalsContainer.appendChild(card);
+});
